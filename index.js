e141337d9575e255d8268bb30066d3bbb9564bd0
xideme <151017452+xideme@users.noreply.github.com>
2024-11-19 16:29:14 +0200

    swaggerUI init

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = require('express')();
 const port = 8080;
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./docs/swagger.json');
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.listen(port, () => {console.log
-('Backed api address: http://localhost:${port}');});
+(`Backend api address: http://localhost:${port}`);});
