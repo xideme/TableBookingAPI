811441246ac0bb4cff01b2d4fd3ac6dc7ba05379
Opilane TTHK <opilane@tthk.ee>
2024-11-25 10:03:52 +0200

    app.get('/clients')

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const port = 8080;
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get('/clients', (req, res) => {
+    res.send(["Nikita Pupkin", "Mart Mets"])
+})
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {console.log
 (`Backend api address: http://localhost:${port}`);});
