b2ceaa74cf7cade2d32cec8e655e9f7208753eae
jegor21 <jegorka2002@gmail.com>
2024-12-17 14:50:58 +0200

    ReservationsTable & View

---
+++ tablebookingFrontend/src/components/ReservationsTable.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: "ReservationsTable",
+    props: {items:Array}
+}
+</script>
+
+<template>
+    <table class="table table-striped">
+        <thead class="table-dark">
+            <tr>
+                <th>name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in items" :key="item.id">
+                <td>{{ item.id }}{{ item.client_id }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<style scoped>
+</style>
