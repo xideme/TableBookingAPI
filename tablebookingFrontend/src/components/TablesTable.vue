db2ff22d18a07afe7570f4630313273b36ecd4c5
Opilane TTHK <jegorka2002@gmail.com>
2025-01-06 11:19:50 +0200

    TablesTable 🙂 & TablesView

---
+++ tablebookingFrontend/src/components/TablesTable.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: "TablesTable",
+    props: {items:Array}
+}
+</script>
+
+<template>
+    <table class="table table-striped">
+        <thead class="table-dark">
+            <tr>
+                <th>name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="item in items" :key="item.id">
+                <td>{{ item.id }}{{ item.reservation_id }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<style scoped>
+</style>
