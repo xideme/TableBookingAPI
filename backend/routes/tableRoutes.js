930a364f5eb948a698ac94efcb7f9d49ba858dc2
Opilane TTHK <jegorka2002@gmail.com>
2025-01-06 11:03:26 +0200

    TablesController & tableRoutes

---
+++ backend/routes/tableRoutes.js
@@ -0,0 +1,11 @@
+const TablesController = require("../controllers/TablesController.js");
+
+module.exports = (app) => {
+    app.route("/tables")
+    .get(TablesController.getAll)
+    .post(TablesController.create)
+    app.route("/tables/:id")
+    .get(TablesController.getById)
+    .put(TablesController.editById)
+    .delete(TablesController.deleteById)
+}
