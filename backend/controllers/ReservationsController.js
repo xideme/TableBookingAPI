325fd433116fd369fa498dda9b30f3cdae103c3e
jegor21 <jegorka2002@gmail.com>
2025-01-14 15:53:51 +0200

    Delivers TB-73 https://bookingtargv23.atlassian.net/browse/TB-73?atlOrigin=eyJpIjoiYTFjZmYyYTY3OTU4NDAyMzhjYzI4ODUzNjhmNTY5MDYiLCJwIjoiaiJ9

--- backend/controllers/ReservationsController.js
+++ backend/controllers/ReservationsController.js
@@ -15,3 +15,25 @@ async (req, res) => {
     if (!reservation) { return res.status(404).send({Error: 'Reservation not found'});}
     res.send(reservation);
 }
+
+exports.create =
+async (req, res) => {
+    if (!req.body.client_id || 
+        !req.body.datetime || 
+        !req.body.adult_count || 
+        !req.body.children_count) 
+        {
+            return res.status(400).send({error: 'Missing one or all parameters'});
+        }
+const newReservation = {
+    client_id: req.body.client_id,
+    datetime: req.body.datetime,
+    adult_count: req.body.adult_count,
+    children_count: req.body.children_count
+}
+const createdReservation = await db.reservations.create(newReservation);
+    return res
+    .status(201)
+    .location(`${Utils.getBaseUrl(req)}/reservations/${createdReservation.id}`)
+    .send(createdReservation);
+}
