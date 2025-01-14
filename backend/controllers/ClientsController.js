0a1396106fdafec76f4e9267a389cb127836f156
jegor21 <jegorka2002@gmail.com>
2025-01-14 15:47:23 +0200

    Delivers TB-66 https://bookingtargv23.atlassian.net/browse/TB-66?atlOrigin=eyJpIjoiZjY4OTRlYmVmMzFjNDMzZGE1ZmI0Y2RlM2I5NmJkZGIiLCJwIjoiaiJ9

--- backend/controllers/ClientsController.js
+++ backend/controllers/ClientsController.js
@@ -15,3 +15,26 @@ async (req, res) => {
     if (!client) { return res.status(404).send({Error: 'Client not found'});}
     res.send(client);
 }
+
+exports.create =
+async (req, res) => {
+    if (!req.body.name || 
+        !req.body.phone || 
+        !req.body.email || 
+        !req.body.bonus_level) 
+        {
+            return res.status(400).send({error: 'Missing one or all parameters'});
+        }
+const newClient = {
+    name: req.body.name,
+    phone: req.body.phone,
+    email: req.body.email,
+    bonus_level: req.body.bonus_level
+}
+const createdClient = await db.clients.create(newClient);
+    return res
+    .status(201)
+    .location(`${Utils.getBaseUrl(req)}/clients/${createdClient.id}`)
+    .send(createdClient);
+}
+
