3cd493c0c9d7d449b5a726cd8758259e9d8274e2
jegor21 <jegorka2002@gmail.com>
2025-01-14 15:52:02 +0200

    Delivers TB-74 https://bookingtargv23.atlassian.net/browse/TB-74?atlOrigin=eyJpIjoiNGEwYTAxODZkYmM3NDhiOWE5OTE2MzAyOGEzMzJiMGUiLCJwIjoiaiJ9

--- backend/controllers/ReservationsController.js
+++ backend/controllers/ReservationsController.js
@@ -1,3 +1,10 @@
 const {db} = require('../db');
 const Utils = require('./utils');
 
+exports.getAll =
+async (req, res) => {
+    res
+    .status(200)
+    .send(reservations
+        .map(({id, client_id}) => {return id, client_id}));
+}
