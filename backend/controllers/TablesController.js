04277dac0e4201e7246408b0b8e3714933620584
jegor21 <jegorka2002@gmail.com>
2025-01-14 15:57:36 +0200

    Delivers TB-81 https://bookingtargv23.atlassian.net/browse/TB-81?atlOrigin=eyJpIjoiZGRkMDcyYTQ0NjZhNDE4OWFkM2VjNzI0YzUzZGY4YTkiLCJwIjoiaiJ9

--- backend/controllers/TablesController.js
+++ backend/controllers/TablesController.js
@@ -2,3 +2,10 @@ const {db} = require('../db');
 const tables = require('../models/tables');
 const Utils = require('./utils');
 
+exports.getAll =
+async (req, res) => {
+    res
+    .status(200)
+    .send(tables
+        .map(({id, reservation_id}) => {return id, reservation_id}));
+}
